b4aef889545af4690a2d110fdf19ec0c44277039
It99plusjava <It99plusjava@gmailcom>
2015-08-05 10:36:23 +0100

    submitAdmissionForm(@ModelAttribute("studentName") String name, @ModelAttribute("studentHobby") String hobby)

--- Gontu/src/main/java/com/gontu/StudentAdmissionController.java
+++ Gontu/src/main/java/com/gontu/StudentAdmissionController.java
@@ -1,14 +1,13 @@
  package com.gontu;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentAdmissionController {
+
 
 	@RequestMapping(value="/admissionForm.html", method = RequestMethod.GET)
 	public ModelAndView getAdmissionForm() {
@@ -17,25 +16,21 @@ public class StudentAdmissionController {
 
 		return model;
 	}
-	
-	//@RequestMapping("/submitAdmissionForm.html")
-	//public ModelAndView submitAdmissionForm(@RequestParam("studentName") String name, @RequestParam("studentHobby") String hobby) {
-
-//		ModelAndView model = new ModelAndView("AdmissionSuccess");
-//		model.addObject("msg","Details submitted by you:: Name: "+name+ ", Hobby: " + hobby);
-
-//		return model;
-//	}
 
 	@RequestMapping(value="/submitAdmissionForm.html", method = RequestMethod.POST)
-	public ModelAndView submitAdmissionForm(@RequestParam Map<String,String> reqPar) {
-
-		String name = reqPar.get("studentName");
-		String hobby = reqPar.get("hobby");
+	public ModelAndView submitAdmissionForm(@ModelAttribute("studentName") String name,
+											@ModelAttribute("studentHobby") String hobby) {
+		
+		Student student1 = new Student();
+		student1.setStudentName(name);
+		student1.setStudentHobby(hobby);
 
 		ModelAndView model = new ModelAndView("AdmissionSuccess");
-		model.addObject("msg","Details submitted by you:: Name: "+name+ ", Hobby: " + hobby);
-
+		model.addObject("headerMessage","Gontu College of Engineering, India");
+		model.addObject("student1",student1);
+		
 		return model;
 	}
+	
 }
+
--- Gontu/src/main/webapp/WEB-INF/AdmissionForm.jsp
+++ Gontu/src/main/webapp/WEB-INF/AdmissionForm.jsp
@@ -1,6 +1,9 @@
 <html>
 <body>
-	<h1> STUDENT ADMISSION FORM FOR ENGINEERING COURSES</h1>
+
+	<h1>${headerMessage}</h1>
+
+	<h3> STUDENT ADMISSION FORM FOR ENGINEERING COURSES</h3>
 
 	<form action="/Gontu/submitAdmissionForm.html" method="post">
 		<p>
@@ -9,6 +12,7 @@
 		<p>
 			Student's Hobby : <input type="text" name="studentHobby" />
 		</p>
+		
 		<input type="submit" value="Submit this form by clicking here" />
 	</form>
  
